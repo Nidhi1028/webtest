3bb8365eacc3aa020d4a9a2bf31caa42f664a99c
om-vb <om-vb@om>
2025-04-01 04:06:13 -0400

    add method in class

--- src/test/java/project/webtest/cartclass.java
+++ src/test/java/project/webtest/cartclass.java
@@ -1,10 +1,15 @@
 package project.webtest;
 
 public class cartclass {
+	static void test()
 
+	{
+			System.out.println("testing");
+			}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello");
+		
 	}
 
 }
