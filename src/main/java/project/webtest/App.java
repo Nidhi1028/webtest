dbe157af23050866adb8939449b76f986dc9d59e
om-vb <om-vb@om>
2025-04-01 03:02:02 -0400

    add a cart class

---
+++ src/main/java/project/webtest/App.java
@@ -0,0 +1,10 @@
+package project.webtest;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/project/webtest/cartclass.java
@@ -0,0 +1,10 @@
+package project.webtest;
+
+public class cartclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
